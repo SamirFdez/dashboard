16cbd904b07fe52277af6afdb048ce2cbb35a763
samir <samirfdezc@gmail.com>
2023-07-10 16:46:54 -0400

    Arreglando cosas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Footer } from './components/foooter/footer'
-import { Header } from './components/header/header'
 import { Estacion } from './Views/estaciones/estacion'
-import { LoginForm } from './components/login/login'
+import { Login } from './Views/Login/login'
 
 
 function App() {
 
   return (
     <>
-      <Header/>
         <Routes>
-          <Route path='/' element={<LoginForm/>}/>
+          <Route path='/' element={<Login/>}/>
           <Route path='/estacion' element={<Estacion/>}/>
         </Routes>
-      <Footer/>
     </>
   )
 }
--- src/Views/estaciones/estacion.jsx
+++ src/Views/estaciones/estacion.jsx
@@ -1,14 +1,18 @@
 import { CantidadDeTurnos } from "../../components/cantidadDeTurnos/cantidadDeTurnos"
 import { EstatusColaboradores } from "../../components/estatusColaboradores/estatusColaboradores"
 import { EstatusEstaciones } from "../../components/estatusEstaciones/estatusEstaciones"
+import { Footer } from "../../components/footer/footer"
+import { Header } from "../../components/header/header";
 
 export const Estacion = function() {
 
   return (   
-      <>
+      <>  
+        <Header/>
         <CantidadDeTurnos/>
         <EstatusEstaciones/>
         <EstatusColaboradores/>
+        <Footer/>
       </>
 )
  
