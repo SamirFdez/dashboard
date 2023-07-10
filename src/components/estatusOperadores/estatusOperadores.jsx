a0cc5d41d483ccffad8812f06c631b8f07fc1683
samir <samirfdezc@gmail.com>
2023-07-10 17:46:39 -0400

    Seguimos agregando

--- src/components/estatusOperadores/estatusOperadores.jsx
+++ src/components/estatusOperadores/estatusOperadores.jsx
@@ -1,4 +1,4 @@
-import { Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import estatusOperadoresStyles  from "./estatusOperadores.module.css"
 
 
@@ -15,10 +15,17 @@ export const EstatusOperadores = function() {
             </Row>
             
             <Row style={{marginBottom: "1em"}}>
-                <Col></Col>
-                <Col lg={10} md={10} sm={10} xs={10} className= { estatusOperadoresStyles.colSubTitle } style={{ marginRight: "0.5em"}}>
+                <Col xl={3} lg={3} md={3} sm={3} xs= {4} className= { estatusOperadoresStyles.card }>
+                    <Row>
+                    <h3 style={{color: "white"}}>Servicio</h3> 
+                    </Row>
+                    <Container>
+                    <Row>
+                        <Col className= { estatusOperadoresStyles.cardBody } style={{marginRight: "0.4em"}}> 2 T </Col>
+                        <Col className= { estatusOperadoresStyles.cardBody }> 32 min </Col>
+                    </Row>
+                    </Container>
                 </Col>
-                <Col></Col>
             </Row>
 
         </>
