b3c6e9af80b3608479d2a87793def2824cc290bc
samir <samirfdezc@gmail.com>
2023-07-26 14:25:13 -0400

    ++

--- src/components/configuracionDashboard/configuracionDashboard.jsx
+++ src/components/configuracionDashboard/configuracionDashboard.jsx
@@ -1,7 +1,7 @@
 import { Container, Form, Row, Col, Button, Dropdown } from 'react-bootstrap'
 import configuracionDashboardStyles  from "./configuracionDashboard.module.css"
 import React from 'react';
-
+import { FaMoon, FaSun } from "react-icons/fa6";
 
 export const ConfigDashboard = function() {
 
@@ -16,37 +16,61 @@ export const ConfigDashboard = function() {
 
             <Container>
                 <Form className={configuracionDashboardStyles.form}>
+                
+                <Container>
+                    <Row className={configuracionDashboardStyles.formRow}>
+                        <Col lg={4} md={6} sm={12} xs={12} style={{marginTop:"1em"}}>
+                            <label className= {configuracionDashboardStyles.labelForm}> Tiempo de actualización de datos </label>
+                        </Col>
 
-                    <Form.Group className={configuracionDashboardStyles.formGroup}>
-                        <Form.Label className= {configuracionDashboardStyles.labelForm}>Tiempo de actualización de datos</Form.Label>
-                        <Dropdown>
-                        <Dropdown.Toggle variant="primary" style={{fontSize:"1.5em"}}>
-                            Tiempo por defecto
-                        </Dropdown.Toggle>
+                        <Col lg={8} md={6} sm={12} xs={12} style={{marginTop:"1em"}}>
+                            <Dropdown>
+                                <Dropdown.Toggle variant="light" style={{fontSize:"1.5em", width:"100%"}}>
+                                    Tiempo
+                                </Dropdown.Toggle>
 
-                        <Dropdown.Menu style={{fontSize:"1.5em"}}>
-                            <Dropdown.Item href="#/action-1">1 Minuto</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">2 Minutos</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">3 Minutos</Dropdown.Item>
-                            <Dropdown.Item href="#/action-4">5 Minutos</Dropdown.Item>
-                            <Dropdown.Item href="#/action-5">10 Minutos</Dropdown.Item>
+                                <Dropdown.Menu style={{fontSize:"1.5em"}}>
+                                    <Dropdown.Item href="">1 Minuto</Dropdown.Item>
+                                    <Dropdown.Item href="">2 Minutos</Dropdown.Item>
+                                    <Dropdown.Item href="">3 Minutos</Dropdown.Item>
+                                    <Dropdown.Item href="">5 Minutos</Dropdown.Item>
+                                    <Dropdown.Item href="">10 Minutos</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
+                    </Row>
 
-                        </Dropdown.Menu>
-                        </Dropdown>
-                    </Form.Group>
+                    <Row className={configuracionDashboardStyles.formGroup} style={{marginBottom: "3em"}}>
+                    <Col lg={4} md={6} sm={12} xs={12} style={{marginTop:"1em"}}>
+                            <label className= {configuracionDashboardStyles.labelForm}> Tema del dasboard </label>
+                        </Col>
 
-                    <Form.Group className={configuracionDashboardStyles.formGroup}>
-                    <Form.Label className= {configuracionDashboardStyles.labelForm}>Tiempo de actualización de datos</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+                        <Col lg={8} md={6} sm={12} xs={12} style={{marginTop:"1em"}}>
+                            <Dropdown>
+                                <Dropdown.Toggle variant="light" style={{fontSize:"1.5em", width:"100%"}}>
+                                    Tema 
+                                </Dropdown.Toggle>
 
-                    <Button variant="danger" type="submit" style={{fontSize:"1.5em", marginRight:"1em"}}>
-                        Cancelar
-                    </Button>
+                                <Dropdown.Menu style={{fontSize:"1.5em"}}>
+                                    <Dropdown.Item href="" style={{fontSize:"1em"}}><span><FaSun/></span> Modo light</Dropdown.Item>
+                                    <Dropdown.Item href="" style={{fontSize:"1em"}}><span><FaMoon/></span> Modo dark</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
+                    </Row>
 
-                    <Button variant="success" type="submit" style={{fontSize:"1.5em"}}>
-                        Guardar
-                    </Button>
+                    <div style={{textAlign:"right"}}>
+                        <Button variant="danger" style={{fontSize:"2em", width:"5em", marginRight:"1em"}}>
+                            Cancelar
+                        </Button>
+
+                        <Button variant="success" type="submit" style={{fontSize:"2em", width:"5em"}}>
+                            Guardar
+                        </Button>
+                    </div>
+
+                    
+                </Container>
                 </Form>
             </Container>
         </>
