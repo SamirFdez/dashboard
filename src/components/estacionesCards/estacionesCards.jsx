1dc14be5c6722bc290817645fd388fac32f646fc
samirfdez <samirfdezc@gmail.com>
2023-09-05 12:16:47 -0400

    Casi listo

--- src/components/estacionesCards/estacionesCards.jsx
+++ src/components/estacionesCards/estacionesCards.jsx
@@ -37,78 +37,78 @@ export const EstacionsCards = function() {
                 <Row xl={12} xs={12} className= { EstacionesCardsStyles.colTitle }> 
                     <h1 style={{marginTop: "0.3em", marginBottom: "0.3em"}}>Estatus estaciones </h1>
                 </Row>
-            </div>  
+ 
+                <Row>
+                    <Col xs={12}>
+                        <Row style={{textAlign: "Center", justifyContent: "center"}}>
 
-            <Row>
-                <Col xs={12}>
-                    <Row style={{textAlign: "Center", justifyContent: "center"}}>
+                            {colaboradores.map((colaborador, index) => ( 
+                                <Col md={3} sm={5} xs={11} key={index}
+                                    className = { 
+                                        colaborador.ERROR === 1 ? EstacionesCardsStyles.cardGray 
+                                        : colaborador.ERROR === 2 ? EstacionesCardsStyles.cardGreen
+                                        : colaborador.ERROR === 3 ? EstacionesCardsStyles.cardBlue
+                                        : colaborador.ERROR === 4 ? EstacionesCardsStyles.cardGreenSpecial
+                                        : EstacionesCardsStyles.cardGray 
+                                }> 
 
-                        {colaboradores.map((colaborador, index) => ( 
-                            <Col md={3} sm={5} xs={11} key={index}
-                                className = { 
-                                    colaborador.ERROR === 1 ? EstacionesCardsStyles.cardGray 
-                                    : colaborador.ERROR === 2 ? EstacionesCardsStyles.cardGreen
-                                    : colaborador.ERROR === 3 ? EstacionesCardsStyles.cardBlue
-                                    : colaborador.ERROR === 4 ? EstacionesCardsStyles.cardGreenSpecial
-                                    : EstacionesCardsStyles.cardGray 
-                            }> 
-
-                                <Row className= { EstacionesCardsStyles.rowImgName }>
-                                    <Col xs={4}>
-                                        <img src={`data:image/jpeg;base64,${colaborador.Foto}`} 
-                                        className= { 
-                                            colaborador.ERROR === 3 ? EstacionesCardsStyles.cardImgOffline
-                                            : EstacionesCardsStyles.cardImg}/>
-                                    </Col>
-                                    
-                                    <Col xs={8} 
+                                    <Row className= { EstacionesCardsStyles.rowImgName }>
+                                        <Col xs={4}>
+                                            <img src={`data:image/jpeg;base64,${colaborador.Foto}`} 
                                             className= { 
-                                            colaborador.ERROR === 3 ? EstacionesCardsStyles.cardNameOffline
-                                            : EstacionesCardsStyles.cardName}>
-                                        <h3 style={{marginTop: "1em"}}> {colaborador.NombreEmpleado} </h3>
-                                        <h4 style={{marginTop: "0.2em"}}> {colaborador.GrupoEstacion} </h4>
-                                    </Col>
-                                </Row>
-                                
-                                <Row className= { EstacionesCardsStyles.rowInfo } style={{marginTop: "1em"}}>
-                                    <Col className= { EstacionesCardsStyles.rowEstacion } style={{marginRight: "1em"}}>
-                                        <h1 style={{marginTop: "0.2em"}}> {colaborador.NumeroEstacion} </h1>
-                                        <h2>Estación de {colaborador.GrupoEstacion}</h2>
-                                    </Col>
-                                    
-                                    <Col className= { EstacionesCardsStyles.rowEstacion } >      
-                                        <h4 style={{marginTop: "0.5em"}}> Turno: </h4>
-                                        <h1> {colaborador.Turno} </h1>
-                                        <h4> {colaborador.TiempoGlobal} </h4>
-
-                                    </Col>
-                                </Row>
-
-                                <Row className= { EstacionesCardsStyles.rowPausa } style={{marginTop: "1em"}}>
-                                        <Row >
-                                            <Col xs={7}>
-                                                <h4> {colaborador.EstatusTurno}</h4>
-                                            </Col>
-                                            <Col xs={5}> 
-                                                <h4> {colaborador.TiempoUltimoEstatus} </h4>
-                                            </Col>
-                                        </Row>
-                                </Row>
-
-                                <Row className= { EstacionesCardsStyles.rowCita } style={{marginTop: "1em"}}>
-                                    <Row>
-                                        <Col>
-                                            <h4> {colaborador.NombreCita} </h4>
-                                            <h4> {colaborador.NumeroCita} </h4>
+                                                colaborador.ERROR === 3 ? EstacionesCardsStyles.cardImgOffline
+                                                : EstacionesCardsStyles.cardImg}/>
+                                        </Col>
+                                        
+                                        <Col xs={8} 
+                                                className= { 
+                                                colaborador.ERROR === 3 ? EstacionesCardsStyles.cardNameOffline
+                                                : EstacionesCardsStyles.cardName}>
+                                            <h3 style={{marginTop: "1em"}}> {colaborador.NombreEmpleado} </h3>
+                                            <h4 style={{marginTop: "0.2em"}}> {colaborador.GrupoEstacion} </h4>
                                         </Col>
                                     </Row>
-                                </Row>
-                            </Col>
-                        ))}
-                            
-                    </Row>
-                </Col> 
-            </Row>
+                                    
+                                    <Row className= { EstacionesCardsStyles.rowInfo } style={{marginTop: "1em"}}>
+                                        <Col className= { EstacionesCardsStyles.rowEstacion } style={{marginRight: "1em"}}>
+                                            <h1 style={{marginTop: "0.2em"}}> {colaborador.NumeroEstacion} </h1>
+                                            <h2>Estación de {colaborador.GrupoEstacion}</h2>
+                                        </Col>
+                                        
+                                        <Col className= { EstacionesCardsStyles.rowEstacion } >      
+                                            <h4 style={{marginTop: "0.5em"}}> Turno: </h4>
+                                            <h1> {colaborador.Turno} </h1>
+                                            <h4> {colaborador.TiempoGlobal} </h4>
+
+                                        </Col>
+                                    </Row>
+
+                                    <Row className= { EstacionesCardsStyles.rowPausa } style={{marginTop: "1em"}}>
+                                            <Row >
+                                                <Col xs={7}>
+                                                    <h4> {colaborador.EstatusTurno}</h4>
+                                                </Col>
+                                                <Col xs={5}> 
+                                                    <h4> {colaborador.TiempoUltimoEstatus} </h4>
+                                                </Col>
+                                            </Row>
+                                    </Row>
+
+                                    <Row className= { EstacionesCardsStyles.rowCita } style={{marginTop: "1em"}}>
+                                        <Row>
+                                            <Col>
+                                                <h4> {colaborador.NombreCita} </h4>
+                                                <h4> {colaborador.NumeroCita} </h4>
+                                            </Col>
+                                        </Row>
+                                    </Row>
+                                </Col>
+                            ))}
+                                
+                        </Row>
+                    </Col> 
+                </Row>
+            </div>  
         </>
     )
 }  
--- src/components/estatusOperadores/estatusOperadores.jsx
+++ src/components/estatusOperadores/estatusOperadores.jsx
@@ -38,84 +38,86 @@ export const EstatusOperadores = function() {
                 <Row className= { estatusOperadoresStyles.colTitle }> 
                     <h1 style={{marginTop: "0.3em", marginBottom: "0.3em"}}>Estatus Operadores</h1>
                 </Row>
-            </div>
-            
-            <Col xs={12}>
-            <Row style={{marginBottom: "1em", justifyContent: "Center"}}>
-                           
-                {operadores.map((operador, index) => (
-                    <Col xxl={2} xl={3} lg={4} md={5} sm={10} xs= {10} className= { estatusOperadoresStyles.card } key={index}>
-                        <Row className= { estatusOperadoresStyles.rowImgName }>
-                            <Col xs={5}>
-                                <img src={`data:image/jpeg;base64,${operador.Foto}`} alt="Imagen operador" className= { estatusOperadoresStyles.cardImg }/>
-                            </Col>
-                            <Col xs={7} className= { estatusOperadoresStyles.cardName }> 
-                                <h3 style={{marginTop: "1.2em"}}> {operador.NOMBRE} </h3>
-                                <h4 style={{marginTop: "0.6em"}}> {operador.DESCRIPCION} </h4>
-                            </Col>
-                        </Row>
-                        
-                        <Row className= { estatusOperadoresStyles.rowReporte } style={{marginTop: "1em"}}>
-                            <Row style={{marginTop: "0.5em"}}>
-                                <Col xs={7}>
-                                    <h4> Atendidos</h4>
+
+                <Row>
+                    <Col xs={12}>
+                        <Row style={{marginBottom: "1em", justifyContent: "Center"}}>
+                                    
+                            {operadores.map((operador, index) => (
+                                <Col md={3} sm={5} xs={11} className= { estatusOperadoresStyles.card } key={index}>
+                                    <Row className= { estatusOperadoresStyles.rowImgName }>
+                                        <Col xs={5}>
+                                            <img src={`data:image/jpeg;base64,${operador.Foto}`} alt="Imagen operador" className= { estatusOperadoresStyles.cardImg }/>
+                                        </Col>
+                                        <Col xs={7} className= { estatusOperadoresStyles.cardName }> 
+                                            <h3 style={{marginTop: "1.2em"}}> {operador.NOMBRE} </h3>
+                                            <h4 style={{marginTop: "0.6em"}}> {operador.DESCRIPCION} </h4>
+                                        </Col>
+                                    </Row>
+                                    
+                                    <Row className= { estatusOperadoresStyles.rowReporte } style={{marginTop: "1em"}}>
+                                        <Row style={{marginTop: "0.5em"}}>
+                                            <Col xs={7}>
+                                                <h4> Atendidos</h4>
+                                            </Col>
+                                            <Col xs={2}> 
+                                                <h4> {operador.ATENDIDOS} </h4>
+                                            </Col>
+                                        </Row>
+                                        
+                                        <Row>
+                                            <Col xs={7}>
+                                                <h4> Finalizados</h4>
+                                            </Col>
+                                            <Col xs={2}> 
+                                                <h4> {operador.FINALIZADO} </h4>
+                                            </Col>
+                                        </Row>
+                                        
+                                        <Row>
+                                            <Col xs={7}>
+                                                <h4> Declinados</h4>
+                                            </Col>
+                                            <Col xs={2}> 
+                                                <h4> {operador.DECLINADOS} </h4>
+                                            </Col>
+                                        </Row>
+                                        
+                                        <Row>
+                                            <Col xs={7}>
+                                                <h4> Transferidos</h4>
+                                            </Col>
+                                            <Col xs={2}>
+                                                <h4> {operador.TRANSFERIDOS} </h4>
+                                            </Col>
+                                        </Row>
+                                        
+                                        <Row style={{marginBottom: "0.5em"}}>
+                                            <Col xs={7}>
+                                                <h4> En espera</h4>
+                                            </Col>
+                                            <Col xs={2}> 
+                                                <h4> {operador.ESPERA} </h4>
+                                            </Col>
+                                        </Row>
+                                    </Row>
+                                    
+                                    <Row className= { estatusOperadoresStyles.rowTimeOnline } style={{marginTop: "1em"}}>
+                                        <Row style={{marginTop: "0.5em"}}>
+                                            <Col xs={7}>
+                                                <h4> { "Tiempo " + operador.ACCION.toLowerCase()}:</h4>
+                                            </Col>
+                                            <Col xs={2}> 
+                                                <h4> {operador.TIEMPOLOGUSUARIO} </h4>
+                                            </Col>
+                                        </Row>
+                                    </Row>
                                 </Col>
-                                <Col xs={2}> 
-                                    <h4> {operador.ATENDIDOS} </h4>
-                                </Col>
-                            </Row>
-                            
-                            <Row>
-                                <Col xs={7}>
-                                    <h4> Finalizados</h4>
-                                </Col>
-                                <Col xs={2}> 
-                                    <h4> {operador.FINALIZADO} </h4>
-                                </Col>
-                            </Row>
-                            
-                            <Row>
-                                <Col xs={7}>
-                                    <h4> Declinados</h4>
-                                </Col>
-                                <Col xs={2}> 
-                                    <h4> {operador.DECLINADOS} </h4>
-                                </Col>
-                            </Row>
-                            
-                            <Row>
-                                <Col xs={7}>
-                                    <h4> Transferidos</h4>
-                                </Col>
-                                <Col xs={2}>
-                                    <h4> {operador.TRANSFERIDOS} </h4>
-                                </Col>
-                            </Row>
-                            
-                            <Row style={{marginBottom: "0.5em"}}>
-                                <Col xs={7}>
-                                    <h4> En espera</h4>
-                                </Col>
-                                <Col xs={2}> 
-                                    <h4> {operador.ESPERA} </h4>
-                                </Col>
-                            </Row>
-                        </Row>
-                        
-                        <Row className= { estatusOperadoresStyles.rowTimeOnline } style={{marginTop: "1em"}}>
-                            <Row style={{marginTop: "0.5em"}}>
-                                <Col xs={7}>
-                                    <h4> { "Tiempo " + operador.ACCION.toLowerCase()}:</h4>
-                                </Col>
-                                <Col xs={2}> 
-                                    <h4> {operador.TIEMPOLOGUSUARIO} </h4>
-                                </Col>
-                            </Row>
-                        </Row>
+                            ))}
+                        </Row> 
                     </Col>
-                ))}
-            </Row> 
-            </Col>
+                </Row>
+            </div>
         </>
     )
 }  
