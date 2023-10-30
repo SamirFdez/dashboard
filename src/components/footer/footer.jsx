68e46c33f2a1d8b4b2eb639128e0afcb15daf2fc
Samir Fernandez <samirfdezc@gmail.com>
2023-10-30 11:45:07 -0400

    ++

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,9 +5,9 @@ export const Footer = function () {
     <>
       <section className="d-flex justify-content-between p-2 footer">
         <Container>
-          <footer className="d-flex flex-wrap align-items-center py-1 my-1">
+          <footer className="d-flex align-items-center py-1">
             <div className="col-12 justify-content-center d-flex align-items-center">
-              <span className="mb-3 mb-md-0 textFooter">
+              <span className="textFooter">
               Copyright © 2023 Signos Framework. All rights reserved.
               </span>
             </div>
